8657e945ba8e7632d329a770ba34f6c9fdf9ad45
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2023-10-18 09:47:45 +0700

    Add files via upload

--- thoi_gian.js
+++ thoi_gian.js
@@ -3,14 +3,24 @@ var hourNow = today.getHours();
 var greeting;
 if (hourNow > 0){
     greeting ='Good Morning!';
+    className ="morning";
 }
 else if (hourNow > 12){
     greeting = 'Good afternoon!';
+    className = "afternoon";
 }
 else if (hourNow > 18){
     greeting = 'Good evening!';
+    className = "evening";
 }
 else {
     greeting = 'Welcome';
+    className = "default";
 }
-document.write('<h3>' + greeting + '</h3>');
+var el = document.getElementById('main');
+el.textContent = greeting;
+el.className = className;
+// el.textContent = greeting;
+// var color = document.getElementsByClassName('time')
+// color.className= "blue"; 
+// document.write('<h3>' + greeting + '</h3>');
